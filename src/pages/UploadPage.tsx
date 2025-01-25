bcc12086312ba7701df7764f4bf8476cadaf51be
Daniil Radchenko <daniil.radchenko@student.tuke.sk>
2025-01-25 20:47:02 +0100

    Testing GH Pages

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import { uploadModel } from "../utils/api";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { InputFile } from "@/components/ui/inputfile";
 
 const UploadPage = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -24,11 +33,24 @@ const UploadPage = () => {
   };
 
   return (
-    <div className="p-4">
-      <h1>Upload Model</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-      {status && <p>{status}</p>}
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Card className="w-1/4 max-w-xl flex flex-col items-center">
+        <CardHeader>
+          <CardTitle>Upload Model</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col w-[90%] gap-4 min-w-fit">
+          <InputFile
+            type="file"
+            onChange={handleFileChange}
+            variant={"default"}
+            filename={file || undefined}
+          />
+          <Button onClick={handleUpload} className="w-full">
+            Upload
+          </Button>
+          {status && <p className="text-center">{status}</p>}
+        </CardContent>
+      </Card>
     </div>
   );
 };
