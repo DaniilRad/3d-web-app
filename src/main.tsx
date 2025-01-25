7b8474d00e6edbb61ae7d57b35e15be9455f3268
Daniil Radchenko <daniil.radchenko@student.tuke.sk>
2025-01-25 19:52:12 +0100

    Testing GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./pages/Main";
-import { HashRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
-      <Main />
-    </HashRouter>
+    <Main />
   </React.StrictMode>
 );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import UploadPage from "./UploadPage";
 import ViewerPage from "./ViewerPage";
 
 const Main = () => {
-    return (
-        <Routes>
-          <Route path="/" element={<ViewerPage />} />
-          <Route path="/upload" element={<UploadPage />} />
-        </Routes>
-      );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ViewerPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
-
 
 export default Main;
