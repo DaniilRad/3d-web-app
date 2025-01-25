b9b4673282c9681e5e6790a5d4cd9ed22f172540
Daniil Radchenko <daniil.radchenko@student.tuke.sk>
2025-01-25 20:12:13 +0100

    Testing GH Pages

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Route, Routes, HashRouter as Router } from "react-router";
 import UploadPage from "./UploadPage";
 import ViewerPage from "./ViewerPage";
 
 const Main = () => {
   return (
-    <BrowserRouter basename="/3d-web-app">
+    <Router basename="/3d-web-app">
       <Routes>
         <Route path="/" element={<ViewerPage />} />
         <Route path="/upload" element={<UploadPage />} /> 
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
