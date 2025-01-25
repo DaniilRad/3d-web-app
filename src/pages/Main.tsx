d6138d1576354f4ddfecaf1b16f961cc4efc2e0c
Daniil Radchenko <daniil.radchenko@student.tuke.sk>
2025-01-25 19:59:54 +0100

    Testing GH Pages

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,10 +4,10 @@ import ViewerPage from "./ViewerPage";
 
 const Main = () => {
   return (
-    <BrowserRouter basename="/3d-web-app/">
+    <BrowserRouter basename="/3d-web-app">
       <Routes>
         <Route path="/" element={<ViewerPage />} />
-        <Route path="/upload" element={<UploadPage />} />
+        <Route path="upload" element={<UploadPage />} /> {/* No leading slash */}
       </Routes>
     </BrowserRouter>
   );
